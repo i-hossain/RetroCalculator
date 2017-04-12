7b3379ab914e3181c930d22eeba6280ae0d9a311
connected-ihossain <ihossain@connectedlab.com>
2017-04-12 11:41:09 -0400

    Implemented design layouts and added sound to numbered buttons

---
+++ RetroCalculator/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by Ismail Hossain on 2017-04-12.
+//  Copyright © 2017 Ismail Hossain. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var btnSound: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+    }
+    
+    @IBAction func numberPressed(sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
+
+
+}
+
